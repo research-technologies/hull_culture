3a2ac738d169e9fabce5c5511fe4f6d54281e251
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2021-02-24 00:01:41 +0000

    revert displays_image to use plain old id

--- lib/generators/hull_culture/templates/config/initializers/prepends.rb
+++ lib/generators/hull_culture/templates/config/initializers/prepends.rb
@@ -1,4 +1,5 @@
 Rails.application.config.to_prepare do
   Hyrax::ManifestBuilderService.prepend Hyrax::PrependManifestBuilderService
   Hyrax::WorkShowPresenter.prepend Hyrax::PrependWorkShowPresenter
+  Hyrax::DisplaysImage.prepend Hyrax::PrependDisplaysImage
 end
