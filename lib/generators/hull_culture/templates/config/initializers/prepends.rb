40ac8401b59d7bf069e9242e7e29a0d035fe39fe
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2021-02-24 00:53:18 +0000

    well that sucked

--- lib/generators/hull_culture/templates/config/initializers/prepends.rb
+++ lib/generators/hull_culture/templates/config/initializers/prepends.rb
@@ -1,5 +1,4 @@
 Rails.application.config.to_prepare do
   Hyrax::ManifestBuilderService.prepend Hyrax::PrependManifestBuilderService
   Hyrax::WorkShowPresenter.prepend Hyrax::PrependWorkShowPresenter
-  Hyrax::DisplaysImage.prepend Hyrax::PrependDisplaysImage
 end
